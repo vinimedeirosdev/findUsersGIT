b3634a69b4c26c0a198995d899bcf379fd5aeda2
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2023-03-11 15:28:12 -0300

    Configure React app for deployment to GitHub Pages

---
+++ src/components/User.tsx
@@ -0,0 +1,40 @@
+import { UserProps } from "../types/user";
+
+import { MdLocationPin } from "react-icons/md";
+
+import { Link } from "react-router-dom";
+
+import classes from "./User.module.css";
+
+const User = ({
+  login,
+  avatar_url,
+  location,
+  following,
+  followers,
+}: UserProps) => {
+  return (
+    <div className={classes.user}>
+      <img src={avatar_url} alt={login} />
+      <h2>{login}</h2>
+      {location && (
+        <p className={classes.location}>
+          <MdLocationPin />
+          <span>{location}</span>
+        </p>
+      )}
+      <div className={classes.stats}>
+        <div>
+          <p>Followers:</p>
+          <p className={classes.number}>{followers}</p>
+        </div>
+        <div>
+          <p>Following:</p>
+          <p className={classes.number}>{following}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
